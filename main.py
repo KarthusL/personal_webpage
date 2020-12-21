073b2bfb5fdc89a1a42cb51727559bcc789abb00
Yifu <karthusliu@gmail.com>
2020-12-21 23:43:04 +0800

    Change of pics

--- main.py
+++ main.py
@@ -48,4 +48,6 @@ def message_database():
 
 if __name__ == "__main__":
     db.create_all()
+    ip_address = Flask.request.remote_addr
+    print(ip_address)
     app.run(host="0.0.0.0", port=80, debug=True)
